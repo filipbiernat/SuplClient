5b4834e3271769cfd238e00aa524e5a750c1412e
Filip Biernat <fbiernat@gmail.com>
2021-03-14 20:11:11 +0100

    LPP Assistance Data Queries

--- SuplPos.py
+++ SuplPos.py
@@ -16,3 +16,20 @@ class SuplPos(SuplPdu):
             print('Decoded LPP Provide Assistance Data PDU:')
             pprint.pprint(lpp_pdu)
         return lpp_pdu
+
+    def get_gnss_assistance_data(self):
+        lpp_pdu = self.get_lpp_provide_assistance_data_pdu()
+        return lpp_pdu['lpp-MessageBody'][1][1]['criticalExtensions'][1][1]['a-gnss-ProvideAssistanceData']
+
+    def get_gnss_common_assistance_data(self):
+        return self.get_gnss_assistance_data()['gnss-CommonAssistData']
+
+    def get_gnss_generic_assistance_data(self):
+        return self.get_gnss_assistance_data()['gnss-GenericAssistData']
+
+    def get_gnss_constellations(self):
+        return list(map(lambda elem: elem['gnss-ID']['gnss-id'], self.get_gnss_generic_assistance_data()))
+
+    def get_gnss_generic_assistance_data_element(self, gnss_id):
+        return next((elem for elem in self.get_gnss_generic_assistance_data() if elem['gnss-ID']['gnss-id'] == gnss_id),
+                    None)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # !/usr/bin/python
 
 import asn1tools
+from pprint import pprint
 from Connection import Connection
 from SuplStart import SuplStart
 from SuplResponse import SuplResponse
@@ -9,37 +10,31 @@ from SuplPos import SuplPos
 from SuplPosAck import SuplPosAck
 
 
-def query_assistance_data():
+def query_assistance_data(latitude, longitude, debug=False):
     supl_codec = asn1tools.compile_files(['ASN1Definitions/SUPL.asn',
                                           'ASN1Definitions/ULP.asn',
                                           'ASN1Definitions/ULP-Components.asn'], 'uper')
     lpp_codec = asn1tools.compile_files('ASN1Definitions/LPP.asn', 'uper')
 
-    latitude = 49.883427
-    longitude = 19.492598
-
-    connection = Connection('108.177.126.192', 7276, supl_codec, debug=True)
+    connection = Connection('108.177.126.192', 7276, supl_codec, debug)
 
     supl_start = SuplStart(supl_codec, lpp_codec)
     connection.send(supl_start.name, supl_start.pdu)
-
     received_pdu = connection.receive('SUPLRESPONSE')
     supl_response = SuplResponse(supl_codec, lpp_codec, received_pdu)
     session_id = supl_response.get_session_id()
 
-    supl_pos_init = SuplPosInit(supl_codec, lpp_codec, session_id, latitude, longitude, debug=True)
+    supl_pos_init = SuplPosInit(supl_codec, lpp_codec, session_id, latitude, longitude, debug)
     connection.send(supl_pos_init.name, supl_pos_init.pdu)
-
     received_pdu = connection.receive('SUPLPOS')
-    supl_pos = SuplPos(supl_codec, lpp_codec, received_pdu, debug=True)
+    supl_pos = SuplPos(supl_codec, lpp_codec, received_pdu, debug)
 
     supl_pos_ack = SuplPosAck(supl_codec, lpp_codec, session_id)
     connection.send(supl_pos_ack.name, supl_pos_ack.pdu)
-
     connection.receive('SUPLEND')
 
-    supl_pos.get_lpp_provide_assistance_data_pdu()
+    pprint(supl_pos.get_lpp_provide_assistance_data_pdu())
 
 
 if __name__ == '__main__':
-    query_assistance_data()
+    query_assistance_data(latitude=49.883427, longitude=19.492598, debug=False)
